ed9e63afdf94dd48f7d4c9911afa65b3d939829a
odra <me@lrossetti.com>
2024-01-30 00:19:52 -0300

    mock tests

--- pkgbox/test/conftest.py
+++ pkgbox/test/conftest.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 from click.testing import CliRunner
 
@@ -12,3 +14,16 @@ def tmpdir(tmp_path):
     tmp_path.mkdir(exist_ok=True)
 
     return tmp_path
+
+
+@pytest.fixture
+def fixdir():
+    realpath = os.path.realpath(__file__)
+
+    return os.path.join(os.path.dirname(realpath), 'fixtures')
+
+
+@pytest.fixture
+def manifest_json(fixdir):
+    with open(f'{fixdir}/manifest.json', 'r') as f:
+        return f.read()
--- pkgbox/test/test_image.py
+++ pkgbox/test/test_image.py
@@ -1,6 +1,8 @@
 import os
 import pathlib
 
+import requests_mock
+
 from pkgbox import image
 
 
@@ -15,10 +17,15 @@ def test_from_str():
     assert img == image.Image('registry.fedoraproject.org', 'fedora', '39')
 
 
-def test_info():
+def test_info(manifest_json):
     name = 'registry.fedoraproject.org/fedora:39'
     img = image.from_str(name)
-    info = image.info(img)
+
+    with requests_mock.Mocker() as m:
+        m.get('https://registry.fedoraproject.org/v2/fedora/manifests/39', 
+              headers={'docker-content-digest': 'sha256:718a00fe32127ad01ddab9fc4b7c968ab2679c92c6385ac6865ae6e2523275e4'},
+              text=manifest_json)
+        info = image.info(img)
 
     assert info.schema_version == 1
     assert info.name == 'fedora'
@@ -29,13 +36,21 @@ def test_info():
     assert len(info.signatures) == 1
 
 
-def test_fetch():
+def test_fetch(manifest_json, tmp_path):
+    dest = tmp_path / 'pkgbox-test'
+    dest.mkdir()
     name = 'registry.fedoraproject.org/fedora:39'
-    img = image.from_str(name)
-    info = image.info(img)
-    dest = pathlib.Path('/tmp/pkgbox-test')
+    img = image.from_str(name) 
 
-    image.fetch(img, info, dest)
+    with requests_mock.Mocker() as m:
+        m.get('https://registry.fedoraproject.org/v2/fedora/manifests/39', 
+              headers={'docker-content-digest': 'sha256:718a00fe32127ad01ddab9fc4b7c968ab2679c92c6385ac6865ae6e2523275e4'},
+              text=manifest_json)
+        m.get('https://registry.fedoraproject.org/v2/fedora/blobs/sha256:718a00fe32127ad01ddab9fc4b7c968ab2679c92c6385ac6865ae6e2523275e4', 
+              text='')
+        # calls
+        info = image.info(img)
+        image.fetch(img, info, dest)
 
     for layer in info.layers:
         assert os.path.exists(f'{dest}/{layer.digest}.tar.gz')
